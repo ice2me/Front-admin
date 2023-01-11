3cf8baf5d1407371725a69ad9c994b5ca83df6c1
Serhii <ice2me1989@gmail.com>
2023-01-11 18:53:33 +0200

    1

--- src/utils/makeUrl.js
+++ src/utils/makeUrl.js
@@ -1,7 +1,7 @@
 export const apiBaseUrl =
 	process.env.NODE_ENV === "development"
 		// ? "http://localhost:5000/api/"
-		? "http://185.25.117.182:8080/api"
+		? ("http://185.25.117.182:8080/api" || 'theke.com.ua/api')
 		: `${window.location.origin}/api/`;
 
 export const makeUrl = (urlSuff) => `${apiBaseUrl}/${urlSuff}`;
