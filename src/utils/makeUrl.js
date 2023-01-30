97d67e0430ea3f330e01ed10cfe620033c8a0842
ice2me <ice2me1989@gmail.com>
2023-01-30 22:31:16 +0200

    fix link

--- src/utils/makeUrl.js
+++ src/utils/makeUrl.js
@@ -1,6 +1,6 @@
 export const apiBaseUrl =
 	process.env.NODE_ENV === "development"
-		? "http://localhost:8080/api/"
+		? "http://backend:8080/api/"
 		// ? "http://185.25.117.182/api"
 		: `${window.location.origin}/api/`;
 // || 'theke.com.ua/api
